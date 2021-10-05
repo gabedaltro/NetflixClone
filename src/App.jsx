21e879bfe798260f5b1513714c5fe9b3d646032e
Gabriel Guimarães <gabeguimaraes13@gmail.com>
2021-10-05 10:26:31 -0300

    all

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
+
+export default function App() {
+
+  const [movieList, setMovieList] = useState([]);
+  const [FeaturedData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
+
+  useEffect(() => { // Quando a tela for carregada, vai execturar tal função
+    const loadAll = async () => { // async = esperar resultado
+      // Pegando a lista TOTAL
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      // Pegando o featured
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
+    }
+
+    loadAll();
+  }, []);
+
+  useEffect(() => {
+    const scrollListener = () => {
+      if(window.scrollY > 10) { // Y é vertical
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    }
+    
+    window.addEventListener('scroll', scrollListener);
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    }
+  }, []);
+
+  return (
+    <div className="page">
+
+    <Header black={blackHeader} />
+
+      {FeaturedData &&
+        <FeaturedMovie item={FeaturedData} />
+      }
+      <section className="lists">
+        {movieList.map((item, key) => ( // Loop nas arrays pra listar cada tipo -- só passa key se for loop
+          <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+      </section>
+
+      <footer>
+        Feito com <span role="img" aria-label="coração">❤</span> por Gabriel Guimarães <br />
+        Direitos de imagem para Netflix <br />
+        Dados pegos do site Themoviedb.org
+      </footer>
+        
+      {movieList.length <= 0 &&
+        <div className="loading">
+          <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="Carregando" />
+        </div>
+      }
+    </div>
+  );
+}
